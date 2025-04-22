f0d42c59d49c5acebacac2178beb63f6f3e51d8c
bouramadevweb <bouramadevweb@gmail.com>
2025-04-22 15:12:16 +0200

    modifie

--- hello_cloud.py
+++ hello_cloud.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
-import http.server
-import socketserver
-import sys
+# import http.server
+# import socketserver
+# import sys
 
-PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
+# PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
 
-Handler = http.server.SimpleHTTPRequestHandler
+# Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"🚀 Serveur en cours d'exécution sur le port {PORT}...")
-    httpd.serve_forever()
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print(f"🚀 Serveur en cours d'exécution sur le port {PORT}...")
+#     httpd.serve_forever()
